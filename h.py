a184865871baa391046b489ca0421f22f42c70da
Zahar <tuzat369@gmail.com>
2022-01-12 09:01:05 +0300

    add letter h

---
+++ h.py
@@ -0,0 +1,14 @@
+def h():
+    h = ['hh      hh',
+'hh      hh',
+'hh      hh',
+'hh      hh',
+'hh      hh',
+'hhhhhhhhhh',
+'hhhhhhhhhh',
+'hh      hh',
+'hh      hh',
+'hh      hh',
+'hh      hh',
+'hh      hh']
+    return h
