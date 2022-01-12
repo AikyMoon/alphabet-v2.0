b647e02349a4dad66d5aaee5f1c34fbbcdb22db3
Zahar <tuzat369@gmail.com>
2022-01-12 09:45:22 +0300

    add letter x

---
+++ x.py
@@ -0,0 +1,15 @@
+def x():
+    return [
+'xxx    xxx',
+'xxx    xxx',
+' xxx  xxx ',
+' xxx  xxx ',
+'  xxxxxx  ',
+'   xxxx   ',
+'   xxxx   ',
+'  xxxxxx  ',
+' xxx  xxx ',
+' xxx  xxx ',
+'xxx    xxx',
+'xxx    xxx' 
+    ]
